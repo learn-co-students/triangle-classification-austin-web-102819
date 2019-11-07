30643114b01a0aabbc9bb47d483390df743050e6
github username <github email address>
2019-11-07 10:59:27 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :l1, :l2, :l3
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+  def kind
+    validate_triangle
+    if l1 == l2 && l1 ==l3
+      :equilateral
+    elsif l1 == l2 || l1 == l3 || l2 == l3
+      :isosceles 
+    elsif l1 != l2 && l1 != l3 && l2 != l3
+      :scalene 
+    end
+  end
+  def validate_triangle
+    real = [(l1 + l2 > l3),(l1 + l3 > l2),(l3 + l2 > l1)]
+    [l1,l2,l3].each {|side| real << false if side <= 0}
+    raise TriangleError if real.include?(false)
+  end
+
+  class TriangleError < StandardError
+     
+  end
 end
